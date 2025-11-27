84743dda7b76b2985098556d77a8bc4e0bda85ab
Gaurav Singh <thakurgaurav1305@gmail.com>
2025-11-28 00:23:39 +0530

    Merged master into main and resolved file conflicts

---
+++ sumof2num.c
@@ -0,0 +1,12 @@
+// #include<stdio.h>
+// #include<conio.h>
+
+// int main()
+// {
+//     int a = 20;
+//     int b = 20;
+//     int c = a+b;
+//     printf("Sum of 2 numbers is : %d",c);
+//     return 0;
+//     getch();
+// }
