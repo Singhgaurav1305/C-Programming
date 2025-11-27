b572857e4b45808046bbffcc9375678eae4cca3e
Gaurav Singh <thakurgaurav1305@gmail.com>
2025-11-28 01:55:50 +0530

    user input added

--- sumof2num.c
+++ sumof2num.c
@@ -10,3 +10,22 @@
 //     return 0;
 //     getch();
 // }
+
+
+//user input
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("Enter a : ");
+    scanf("%d",&a);
+    printf("Enter b : ");
+    scanf("%d",&b);
+    c = a+b;
+    printf("The sum of %d and %d is %d",c);
+    return 0;
+    getch();
+}
