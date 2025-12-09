14a9f860198091d7b993a05b66cdd36e67cedcc6
Gaurav Singh <thakurgaurav1305@gmail.com>
2025-12-09 20:50:52 +0530

    square of number

--- userinput.c
+++ userinput.c
@@ -4,6 +4,6 @@ int main()
     int x;
     printf("Enter the value of x : ");
     scanf("%d",&x);
-    printf("The value of x is %d",x);
+    printf("The value of x is %d and it's square is %d",x,x*x);
 
 }
