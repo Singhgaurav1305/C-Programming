5b18e9c82427e37b5a535268ab99555f105874c3
Gaurav Singh <thakurgaurav1305@gmail.com>
2025-12-10 19:01:48 +0530

    post++ user input added

--- postincrement.c
+++ postincrement.c
@@ -1,7 +1,18 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int x = 10,y;
+//     y=x++; //The priority of the post-increment operator is lower than other operators, especially the assignment operator.
+//     printf("%d %d",x,y);
+// }
+
+//user input
 #include<stdio.h>
 int main()
 {
-    int x = 10,y;
-    y=x++; //The priority of the post-increment operator is lower than other operators, especially the assignment operator.
-    printf("%d %d",x,y);
+    int x,y;
+    printf("Enter value of x : ");
+    scanf("%d",&x);
+    y=x++;
+    printf("post increment is : %d %d",x,y);
 }
