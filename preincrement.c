a04474c56edc5691358412f842fa4b44bedc86f8
Gaurav Singh <thakurgaurav1305@gmail.com>
2025-12-10 19:15:22 +0530

    ++pre user input also

--- preincrement.c
+++ preincrement.c
@@ -1,7 +1,19 @@
+// #include<stdio.h>
+// int main()
+// {
+//    int x = 12,y;
+//    y = ++x; //The priority of the pre-increment operator is higher than the assignment operator.
+//    printf("%d %d",x,y);
+// }
+
+// user input
+
 #include<stdio.h>
 int main()
 {
-    int x = 10,y;
-    y=x++; //The priority of the post-increment operator is lower than other operators, especially the assignment operator.
-    printf("%d %d",x,y);
+    int a,b;
+    printf("Enter the value of a : ");
+    scanf("%d",&a);
+    b=++a;
+    printf("The pre increment is : %d %d",a,b);
 }
